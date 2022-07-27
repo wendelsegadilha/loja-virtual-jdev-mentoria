18b65b2385b2a3761791aa174cedaf05ce63ab4a
Desenvolvimento <Desenvolvimento@DESKTOP-O6GLFHU>
2022-07-27 11:24:39 -0300

    criada classe de modelo CategoriaProduto

--- src/main/java/xyz/wendelsegadilha/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/xyz/wendelsegadilha/lojavirtual/model/CategoriaProduto.java
@@ -1,6 +1,5 @@
 package xyz.wendelsegadilha.lojavirtual.model;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -12,31 +11,29 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable{
-
-	private static final long serialVersionUID = 1L;
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
 	
 	@Column(nullable = false)
 	private String nomeDesc;
-	
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getNomeDesc() {
 		return nomeDesc;
 	}
-	
+
 	public void setNomeDesc(String nomeDesc) {
 		this.nomeDesc = nomeDesc;
 	}
@@ -54,7 +51,7 @@ public class MarcaProduto implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		return Objects.equals(id, other.id);
 	}
 	
