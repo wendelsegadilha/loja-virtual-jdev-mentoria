cc7b8231c997e7fb45e19efc10e583505a1cd455
Desenvolvimento <Desenvolvimento@DESKTOP-O6GLFHU>
2022-07-28 16:02:55 -0300

    criado Enum tipoEndereco

---
+++ src/main/java/xyz/wendelsegadilha/lojavirtual/enums/TipoEndereco.java
@@ -0,0 +1,23 @@
+package xyz.wendelsegadilha.lojavirtual.enums;
+
+public enum TipoEndereco {
+	
+	COBRANCA("Cobrança"),
+	ENTREGA("Entrega");
+	
+	private String descricao;
+	
+	private TipoEndereco(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+
+}
