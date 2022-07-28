1a6d1dcc10cf52b5b397511678561b8fa0f966c8
Desenvolvimento <Desenvolvimento@DESKTOP-O6GLFHU>
2022-07-28 15:36:15 -0300

    criado classe modelo de Endereco

--- src/main/java/xyz/wendelsegadilha/lojavirtual/model/Pessoa.java
+++ src/main/java/xyz/wendelsegadilha/lojavirtual/model/Pessoa.java
@@ -1,14 +1,19 @@
 package xyz.wendelsegadilha.lojavirtual.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
 @Entity
@@ -25,6 +30,9 @@ public abstract class Pessoa implements Serializable{
 	private String nome;
 	private String email;
 	private String telefone;
+	
+	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Endereco> enderecos = new ArrayList<>();
 	
 	public Long getId() {
 		return id;
@@ -50,6 +58,13 @@ public abstract class Pessoa implements Serializable{
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
