87a80315f3a7cde194b8c218e2e3c8681ad54a72
Desenvolvimento <Desenvolvimento@DESKTOP-O6GLFHU>
2022-07-31 11:18:19 -0300

    criada classe de modelo ImagemProduto

---
+++ src/main/java/xyz/wendelsegadilha/lojavirtual/model/ImagemProduto.java
@@ -0,0 +1,88 @@
+package xyz.wendelsegadilha.lojavirtual.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "imagem_produto")
+@SequenceGenerator(name = "seq_imagem_produto", sequenceName = "seq_imagem_produto", allocationSize = 1, initialValue = 1)
+public class ImagemProduto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_imagem_produto")
+	private Long id;
+	
+	@Column(columnDefinition = "text")
+	private String imagemOriginal;
+	
+	@Column(columnDefinition = "text")
+	private String imagemMinhatura;
+	
+	@ManyToOne(targetEntity = Produto.class)
+	@JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+	private Produto produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getImagemOriginal() {
+		return imagemOriginal;
+	}
+
+	public void setImagemOriginal(String imagemOriginal) {
+		this.imagemOriginal = imagemOriginal;
+	}
+
+	public String getImagemMinhatura() {
+		return imagemMinhatura;
+	}
+
+	public void setImagemMinhatura(String imagemMinhatura) {
+		this.imagemMinhatura = imagemMinhatura;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ImagemProduto other = (ImagemProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
