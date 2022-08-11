99c94f73d73acec69906ce37bc974aa87b5c196f
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-08-11 14:51:58 -0300

    anotacoes essenciais na classe principal

--- src/main/java/xyz/wendelsegadilha/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/xyz/wendelsegadilha/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,16 @@ package xyz.wendelsegadilha.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "xyz.wendelsegadilha.lojavirtual.model")
+@ComponentScan(basePackages = "xyz.wendelsegadilha.*")
+@EnableJpaRepositories(basePackages = "xyz.wendelsegadilha.lojavirtual.repository")
+@EnableTransactionManagement
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
