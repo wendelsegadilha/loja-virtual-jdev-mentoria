555d884b8381c66a5c0663154579c045d610aa5c
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-09-06 21:59:14 -0300

    adicionado enderecos ao teste de cadastro de pessoa juridica

--- src/test/java/xyz/wendelsegadilha/lojavirtual/TestePessoaUsuario.java
+++ src/test/java/xyz/wendelsegadilha/lojavirtual/TestePessoaUsuario.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Profile;
 
 import junit.framework.TestCase;
 import xyz.wendelsegadilha.lojavirtual.controller.PessoaController;
+import xyz.wendelsegadilha.lojavirtual.enums.TipoEndereco;
+import xyz.wendelsegadilha.lojavirtual.model.Endereco;
 import xyz.wendelsegadilha.lojavirtual.model.PessoaJuridica;
 
 @Profile("test")
@@ -23,15 +25,51 @@ public class TestePessoaUsuario extends TestCase {
 
 		PessoaJuridica pessoaJuridica = new PessoaJuridica();
 		pessoaJuridica.setCnpj(""+Calendar.getInstance().getTimeInMillis());
-		pessoaJuridica.setNome("Alex fernando");
-		pessoaJuridica.setEmail("alex.fernando.egidio@gmail.com");
-		pessoaJuridica.setTelefone("45999795800");
-		pessoaJuridica.setInscEstadual("65556565656665");
-		pessoaJuridica.setInscMunicipal("55554565656565");
-		pessoaJuridica.setNomeFantasia("54556565665");
-		pessoaJuridica.setRazaoSocial("4656656566");
+		pessoaJuridica.setNome("Alex fernando End");
+		pessoaJuridica.setEmail("teste.enderecos.save2@gmail.com");
+		pessoaJuridica.setTelefone("45945695800");
+		pessoaJuridica.setInscEstadual("6555654565656665");
+		pessoaJuridica.setInscMunicipal("5555456435656565");
+		pessoaJuridica.setNomeFantasia("5455653465665");
+		pessoaJuridica.setRazaoSocial("465665556566");
+		
+		Endereco endereco1 = new Endereco();
+		endereco1.setBairro("Jd Dias");
+		endereco1.setCep("556556565");
+		endereco1.setComplemento("Casa cinza");
+		endereco1.setEmpresa(pessoaJuridica);
+		endereco1.setNumero("389");
+		endereco1.setPessoa(pessoaJuridica);
+		endereco1.setRuaLogra("Av. são joao sexto");
+		endereco1.setTipoEndereco(TipoEndereco.COBRANCA);
+		endereco1.setUf("PR");
+		endereco1.setCidade("Curitiba");
+		
+		
+		Endereco endereco2 = new Endereco();
+		endereco2.setBairro("Jd Maracana");
+		endereco2.setCep("7878778");
+		endereco2.setComplemento("Andar 4");
+		endereco2.setEmpresa(pessoaJuridica);
+		endereco2.setNumero("555");
+		endereco2.setPessoa(pessoaJuridica);
+		endereco2.setRuaLogra("Av. maringá");
+		endereco2.setTipoEndereco(TipoEndereco.ENTREGA);
+		endereco2.setUf("PR");
+		endereco2.setCidade("Curitiba");
+		
+		pessoaJuridica.getEnderecos().add(endereco2);
+		pessoaJuridica.getEnderecos().add(endereco1);
 
-		pessoaController.salvarPj(pessoaJuridica);
+		pessoaJuridica = pessoaController.salvarPj(pessoaJuridica).getBody();
+		
+		assertEquals(true, pessoaJuridica.getId() > 0 );
+		
+		for (Endereco endereco : pessoaJuridica.getEnderecos()) {
+			assertEquals(true, endereco.getId() > 0);
+		}
+		
+		assertEquals(2, pessoaJuridica.getEnderecos().size());
 	}
 
 }
