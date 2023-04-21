d3ed0f0cf6e9917e3a5107b97fec0fa70020082f
dev <dev@SENSORWEB-PC>
2023-04-21 15:49:53 -0300

    projeto criado

---
+++ src/main/java/br/com/wendelsegadilha/lojavirtualjdevmentoria/LojaVirtualJdevMentoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.wendelsegadilha.lojavirtualjdevmentoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualJdevMentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualJdevMentoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wendelsegadilha/lojavirtualjdevmentoria/LojaVirtualJdevMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wendelsegadilha.lojavirtualjdevmentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualJdevMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
