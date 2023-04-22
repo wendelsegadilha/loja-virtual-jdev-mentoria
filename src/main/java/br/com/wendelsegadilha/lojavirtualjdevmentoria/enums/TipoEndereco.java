4ad111daa2a7834f775f8b43192cac66ec33a81e
dev <dev@SENSORWEB-PC>
2023-04-22 19:27:33 -0300

    criado TipoEndereco, Usuario mapeado usuarios_acesso

---
+++ src/main/java/br/com/wendelsegadilha/lojavirtualjdevmentoria/enums/TipoEndereco.java
@@ -0,0 +1,22 @@
+package br.com.wendelsegadilha.lojavirtualjdevmentoria.enums;
+
+public enum TipoEndereco {
+	
+	COBRANCA("Cobrança"),
+	ENTREGA("Entrega");
+	
+	private String descricao;
+	
+	TipoEndereco(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+}
--- src/main/java/br/com/wendelsegadilha/lojavirtualjdevmentoria/model/Endereco.java
+++ src/main/java/br/com/wendelsegadilha/lojavirtualjdevmentoria/model/Endereco.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +14,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import br.com.wendelsegadilha.lojavirtualjdevmentoria.enums.TipoEndereco;
 
 @Entity
 @Table(name = "endereco")
@@ -34,6 +38,9 @@ public class Endereco implements Serializable {
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoEndereco tipoEndereco;
 
 	public Long getId() {
 		return id;
@@ -105,6 +112,14 @@ public class Endereco implements Serializable {
 
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
+	}
+	
+	public TipoEndereco getTipoEndereco() {
+		return tipoEndereco;
+	}
+
+	public void setTipoEndereco(TipoEndereco tipoEndereco) {
+		this.tipoEndereco = tipoEndereco;
 	}
 
 	@Override
